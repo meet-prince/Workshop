d721d3fa53e00c54277964b815cd2a4f7f13cfbb
Prince <pagehub.prince@gmail.com>
2025-07-14 14:48:34 +0530

    If, elif , else and or

--- CS50/conditional/compare.py
+++ CS50/conditional/compare.py
@@ -1,1 +1,33 @@
+x = int(input("What's x?"))
+y = int(input("What's y?"))
 
+
+# if x < y:
+#     print("x is less than y.")
+
+# if x > y:
+#     print("y is less than x.")
+
+# if x == y:
+#     print("x is equal to y.")
+# if x < y:
+#     print("x is less than y.")
+
+# elif x > y:
+#     print("y is less than x.")
+
+# elif x == y:
+#     print("x is equal to y.")
+
+# elif x == y:
+#     print("x is equal to y.") 
+
+#directly asking is x equal to y.
+# if x > y or x < y:
+#     print("x is not equal to y.")
+#Instead of asking upper condition for checking equality, we can use the not equal operator.
+if x != y:
+    print("x is not equal to y.")
+
+else:
+    print("x is equal to y.")
